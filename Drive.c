fde49f0874d4cd209855aca5d27e3a74860d45b5
FenixAuriga <90572005+FenixAuriga@users.noreply.github.com>
2021-09-21 22:51:55 +0200

    Create Drive.c

---
+++ Drive.c
@@ -0,0 +1,55 @@
+/* ======================================================= */
+
+#include <stdio.h>
+#include "brick.h"
+#include <unistd.h>
+
+#define Sleep( msec ) usleep(( msec ) * 1000 ) /* Definerar sleep, Sleep(1000)= 1 sekund */
+
+#define MOTOR_RIGHT    	OUTB
+#define MOTOR_LEFT    	OUTC
+#define MOTOR_A    		OUTA
+#define MOTOR_D    		OUTD
+#define SENSOR_TOUCH	IN1
+#define SENSOR_2		IN2
+#define SENSOR_3		IN3
+#define SENSOR_4		IN4
+
+#define MOTOR_BOTH     	( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+
+int max_hastighet;         /* variabel för max hastighet på motorn */
+
+int main( void )
+{  
+	
+	if ( !brick_init()) return ( 1 ); /* Initialiserar EV3-klossen */
+	printf( "*** ( EV3 ) Hello! ***\n" );
+	Sleep( 2000 );
+	
+	if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* TACHO_TYPE__NONE_ = Alla typer av motorer */
+        max_hastighet = tacho_get_max_speed( MOTOR_LEFT, 0 );	/* Kollar maxhastigheten som motorn kan ha */
+        tacho_reset( MOTOR_BOTH );
+    } else {
+        printf( "Anslut vänster motor i port C,\n"
+        "Anslut höger motor i port B.\n"
+        );
+      	brick_uninit();
+        return ( 0 );  /* Stänger av sig om motorer ej är inkopplade */
+    }
+	
+
+	tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.5 );  // Sätter hastigheten på båda motorerna till 50% av maxhastigheten
+	/* Om man vill köra bakåt anger man negativ hastighet, till exempel max_hastighet * (-0.5) */
+  
+  tacho_set_position_sp( MOROR_BOTH, 0);
+	
+	tacho_run_to_rel_pos(  MOTOR_BOTH, 10 );
+	Sleep( 2000 );  
+	
+	
+	
+	brick_uninit();
+	printf( "dying...\n" );
+    return ( 0 );
+
+}
